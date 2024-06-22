bffb6745dec12ce97d9680b2721fa082adc14719
Song_ANT <songhagyeom@gmail.com>
2024-06-22 15:02:31 +0900

    [Feat] Q_25083
    새싹 언어 제한
    시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
    1 초	1024 MB	133710	68924	63855	52.826%
    문제
    아래 예제와 같이 새싹을 출력하시오.
    
    입력
    입력은 없다.
    
    출력
    새싹을 출력한다.
    
    예제 입력 1
    예제 출력 1
             ,r'"7
    r`-_   ,'  ,/
     \. ". L_r'
       `~\/
          |
          |

---
+++ AlgorithmCoding/Q_25083.cs
@@ -0,0 +1,45 @@
+﻿namespace AlgorithmCoding
+{
+    #region 문제
+    /*
+     새싹 언어 제한
+    시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+    1 초	1024 MB	133710	68924	63855	52.826%
+    문제
+    아래 예제와 같이 새싹을 출력하시오.
+
+    입력
+    입력은 없다.
+
+    출력
+    새싹을 출력한다.
+
+    예제 입력 1 
+    예제 출력 1 
+             ,r'"7
+    r`-_   ,'  ,/
+     \. ". L_r'
+       `~\/
+          |
+          |
+     */
+    #endregion
+
+    public class Q_25083 : KataBase
+    {
+        public override void Example()
+        {
+            Solution();   
+        }
+
+        private void Solution()
+        {
+            Console.WriteLine("         ,r'\"7");
+            Console.WriteLine("r`-_   ,'  ,/");
+            Console.WriteLine(" \\. \". L_r'");
+            Console.WriteLine("   `~\\/");
+            Console.WriteLine("      |");
+            Console.WriteLine("      |");
+        }
+    }
+}
